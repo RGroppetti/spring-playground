baf22495f15557a3d0fa219e0ce3a1df027a7b76
Robert Groppetti <robgroppetti@gmail.com>
2020-07-20 10:45:02 -0500

    added pi

--- src/main/java/com/example/demo/PagesController.java
+++ src/main/java/com/example/demo/PagesController.java
@@ -5,9 +5,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PagesController {
-    @GetMapping("/hello")
-    public String hello(){
-         return "Hello World";
+    @GetMapping("/math/pi")
+    public String pi(){
+         return "3.141592653589793";
     }
 
 }
